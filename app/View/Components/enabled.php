9ea5a2fdfc5e8ed41087e9c6b83dbf868b870fe6
cans01dev <web@cans01.jp>
2024-04-13 15:12:04 +0900

    upper case

--- app/View/Components/enabled.php
+++ app/View/Components/enabled.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class enabled extends Component
+class Enabled extends Component
 {
     /**
      * Create a new component instance.

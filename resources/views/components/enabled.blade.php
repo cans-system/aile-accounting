b3f2878c13c15d4bb052d754ebef11549f34d39b
cans01dev <web@cans01.jp>
2024-04-13 15:16:36 +0900

    anonymous

--- resources/views/components/enabled.blade.php
+++ resources/views/components/enabled.blade.php
@@ -1,8 +1,10 @@
+@props(['enabled' => true, 'name' => 'enabled'])
+
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="{{ $name }}" value="1" id="enabled" @checked($enabled)>
-  <label class="form-check-label" for="enabled">有効</label>
+    <input class="form-check-input" type="radio" name="{{ $name }}" value="1" id="enabled" @checked($enabled)>
+    <label class="form-check-label" for="enabled">有効</label>
 </div>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="{{ $name }}" value="0" id="disabled" @checked(!$enabled)>
-  <label class="form-check-label" for="disabled">利用不可</label>
+    <input class="form-check-input" type="radio" name="{{ $name }}" value="0" id="disabled" @checked(!$enabled)>
+    <label class="form-check-label" for="disabled">利用不可</label>
 </div>
